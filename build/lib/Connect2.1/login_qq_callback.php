b9586d8b24d042e501484c4dd1da9fa51bb69866
SinbadFreedom <151229152@qq.com>
2019-04-29 22:40:14 +0800

    1. 修改q登陆

--- build/lib/Connect2.1/login_qq_callback.php
+++ build/lib/Connect2.1/login_qq_callback.php
@@ -4,11 +4,11 @@ $qc = new QC();
 $access_token = $qc->qq_callback();
 $open_id = $qc->get_openid();
 //$url = 'https://graph.qq.com/user/get_user_info?access_token='. $access_token .'&oauth_consumer_key=101569922&openid='. $open_id .'';
-//echo $access_token;
-//echo '<br>';
-//echo $open_id;
-//echo '<br>';
+echo $access_token;
+echo '<br>';
+echo $open_id;
+echo '<br>';
 $qc_info = new QC($access_token, $open_id);
 $arr = $qc->get_user_info();
-//print_r($arr);
+print_r($arr);
 
