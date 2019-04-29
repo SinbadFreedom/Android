d4a482f1bc97b44351c939b94cc258cc0e4b69ec
SinbadFreedom <151229152@qq.com>
2019-04-29 12:16:17 +0800

    1. 加入QQ登录

--- build/index.php
+++ build/index.php
@@ -14,6 +14,9 @@
 </div>
 
 <div class="container">
+    <div style="float: right">
+        <a href="#" onclick='qq_login()'><img src="img/bt_blue_76X24.png"></a>
+    </div>
     <div class="col-sm-6 col-md-4 col-lg-4 text-center">
         <a href="python3.7.2/zh_cn/catalog.php"><h2><br>Python3.7.2<br><br></h2></a>
     </div>
@@ -46,5 +49,14 @@
     });
 </script>
 
+<script>
+    function qq_login() {
+        alert(1);
+        //以下为按钮点击事件的逻辑。注意这里要重新打开窗口
+        //否则后面跳转到QQ登录，授权页面时会直接缩小当前浏览器的窗口，而不是打开新窗口
+        var A = window.open("lib/Connect2.1/index.php", "TencentLogin",
+            "width=450,height=320,menubar=0,scrollbars=1, resizable = 1, status = 1, titlebar = 0, toolbar = 0, location = 1");
+    }
+</script>
 </body>
 </html>
---
+++ build/lib/Connect2.1/API/comm/inc.php
@@ -0,0 +1,2 @@
+<?php die('forbidden'); ?>
+{"appid":"101569922","appkey":"dd74c21d923d31c3faaf3ad48b947e0c","callback":"https://localhost/login_qq_callback.php","scope":"get_user_info,add_share,list_album,add_album,upload_pic,add_topic,add_one_blog,add_weibo,check_page_fans,add_t,add_pic_t,del_t,get_repost_list,get_info,get_other_info,get_fanslist,get_idolist,add_idol,del_idol,get_tenpay_addr","errorReport":true,"storageType":"file","host":"localhost","user":"root","password":"root","database":"test"}
