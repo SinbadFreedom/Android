1860e702a6af3d14d5624477779dc44f03e92e1e
SinbadFreedom <151229152@qq.com>
2019-05-03 01:06:26 +0800

    1. 修改Q登陆

--- build/php/forum/note_new_ui.php
+++ build/php/forum/note_new_ui.php
@@ -66,7 +66,7 @@ session_start();
     function getUrlParam(name) {
         var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
         var r = window.location.search.substr(1).match(reg);
-        if (r != null) return r[2];
+        if (r != null) return decodeURI(r[2]);
         return null;
     }
 
