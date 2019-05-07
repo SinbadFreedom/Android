27251c3f7d3cd91977dbca87a9913e6db4f90b60
SinbadFreedom <151229152@qq.com>
2019-05-07 23:40:01 +0800

    1. 修改评论翻页

--- build/php/forum/note_get.php
+++ build/php/forum/note_get.php
@@ -66,7 +66,6 @@ $command = new MongoDB\Driver\Command([
 
 $command_cursor = $manager->executeCommand('db_reply', $command);
 /** 笔记总条数 列表分页用*/
-var_dump($command_cursor->toArray());
 $total_count = $command_cursor->toArray()[0]->n;
 echo '--------------';
 echo '<br>';
