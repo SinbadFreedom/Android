6327e91fbc033c00c40bbdc8a715b56fcede147b
SinbadFreedom <151229152@qq.com>
2019-05-07 19:09:47 +0800

    1. 修改变量名

--- build/php/forum/note_get.php
+++ build/php/forum/note_get.php
@@ -135,7 +135,7 @@ foreach ($cursor as $document) {
                 if (isset($_SESSION['figureurl_qq'])) {
                     echo '<a class="nav-link" href="/php/user_info.php"><img class="rounded" src="' . $_SESSION['figureurl_qq'] . '" width="24px" height="24px"></a>';
                 } else {
-                    echo '<a class="nav-link" href="/php/login.php"><b>登录</b></a>';
+                    echo '<a class="nav-link" href="/php/login_ui.php"><b>登录</b></a>';
                 }
                 ?>
             </li>
