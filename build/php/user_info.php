06e9331e50c9fdd57e79d02ec217871b90fbaf06
SinbadFreedom <151229152@qq.com>
2019-05-10 16:49:34 +0800

    1. 修改用户信息页

--- build/php/user_info.php
+++ build/php/user_info.php
@@ -1,5 +1,21 @@
 <?php
 session_start();
+$user_id = $_SESSION['user_id'];
+
+if ($user_id) {
+    $manager = new MongoDB\Driver\Manager("mongodb://localhost:27017");
+    $filter = ['user_id' => $user_id];
+    $options = ['limit' => 1];
+    $query_find = new MongoDB\Driver\Query($filter, $options);
+    $cursor = $manager->executeQuery('db_account.col_user', $query_find);
+    $user_info = $cursor->toArray()[0];
+    var_dump($user_info);
+    $head_img_url = $user_info->headimgurl;
+    $nick_name = $user_info->nickname;
+    $sex = $user_info->sex;
+    $exp = $user_info->exp;
+    $create_time = $user_info->create_time;
+}
 ?>
 <!DOCTYPE html>
 <html lang="zh_CN">
@@ -28,23 +44,30 @@ session_start();
             <li class="nav-item">
                 <a class="nav-link" href="/php/rank_list.php"><b>排行榜</b></a>
             </li>
-            <?php
-            if (isset($_SESSION['figureurl_qq'])) {
-                echo '<a class="nav-link" href="/php/user_info.php"><img class="rounded" src="' . $_SESSION['figureurl_qq'] . '" width="24px" height="24px"></a>';
-            } else {
-                echo '<a class="nav-link" href="/php/login_ui.php"><b>登录</b></a>';
-            }
-            ?>
+        </ul>
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <?php
+                if (isset($_SESSION['figureurl_qq'])) {
+                    echo '<a class="nav-link" href="/php/user_info.php"><img class="rounded" src="' . $_SESSION['figureurl_qq'] . '" width="24px" height="24px"></a>';
+                } else {
+                    echo '<a class="nav-link " href="/php/login_ui.php"><b>登录</b></a>';
+                }
+                ?>
+            </li>
         </ul>
     </div>
 </nav>
 
 <div class="container">
-    <div>
-        <img src="<?php echo $_SESSION['figureurl_qq']; ?>">
+    <div class="mr-auto">
+        <img src="<?php echo $head_img_url ?>">
     </div>
     <div>
-        <span><?php echo $_SESSION['nickname']; ?></span>
+        <span><?php echo $nick_name ?></span>
+    </div>
+    <div>
+        <span><?php echo $exp ?></span>
     </div>
 </div>
 
