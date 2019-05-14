f90ff261272edde47a65e562dbf660516f3d2239
SinbadFreedom <151229152@qq.com>
2019-05-14 21:58:23 +0800

    1. 分离时间工具，统一时间相关的表名

---
+++ build/php/util_time.php
@@ -0,0 +1,26 @@
+<?php
+
+error_reporting(E_ALL ^ E_NOTICE);
+date_default_timezone_set('PRC');
+
+/** 日期相关的key与mongodb表对应*/
+$time_stamp = time();
+$today = date('Y-m-d', $time_stamp);
+$yesterday = date('Y-m-d', $time_stamp - 24 * 60 * 60);
+$year = date('Y', $time_stamp);
+$month = date('m', $time_stamp);
+/** 从1970年1月1日以来的总周数，方便计算上一周排名, 1970年1月1日是周四，减去4天的时间差，从19700105日，周一的时间差开始算总周数*/
+$week = intval(($time_stamp - 4 * 24 * 60 * 60) / (7 * 24 * 60 * 60));
+/** 日,周，月，经验数据表名*/
+$col_today = "col_day_" . $today;
+$col_yesterday = "col_day_" . $yesterday;
+$col_week = "col_week_" . $week;
+$col_week_last = "col_week_" . ($week - 1);
+$col_month = "col_month_" . $year . "_" . $month;
+
+$col_month_last = '';
+if ($month > 1) {
+    $col_month_last = "col_month_" . $year . "_" . $month;
+} else {
+    $col_month_last = "col_month_" . ($year - 1) . "_" . 12;
+}
