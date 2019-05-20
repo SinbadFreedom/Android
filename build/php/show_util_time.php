48c7eb4698a6d7b8d694637f490039c916e9b3d2
SinbadFreedom <151229152@qq.com>
2019-05-20 20:39:50 +0800

    1. 显示时间

--- build/php/show_util_time.php
+++ build/php/show_util_time.php
@@ -2,13 +2,24 @@
 require_once('util_time.php');
 
 echo '$today '. $today;
+echo '<br>';
 echo '$yesterday '. $yesterday;
+echo '<br>';
 echo '$year '. $year;
+echo '<br>';
 echo '$month '. $month;
+echo '<br>';
 echo '$week '. $week;
+echo '<br>';
 echo '$col_today '. $col_today;
+echo '<br>';
 echo '$col_yesterday '. $col_yesterday;
+echo '<br>';
 echo '$col_week '. $col_week;
+echo '<br>';
 echo '$col_week_last '. $col_week_last;
+echo '<br>';
 echo '$col_month '. $col_month;
+echo '<br>';
 echo '$col_month_last '. $col_month_last;
+echo '<br>';
