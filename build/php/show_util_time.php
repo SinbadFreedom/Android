f2c20bf179949b1eb2012df4ae9fbfdb90c3eeab
SinbadFreedom <151229152@qq.com>
2019-05-20 19:52:08 +0800

    1. qq web unionid

---
+++ build/php/show_util_time.php
@@ -0,0 +1,14 @@
+<?php
+require_once('util_time.php');
+
+echo '$today '. $today;
+echo '$yesterday '. $yesterday;
+echo '$year '. $year;
+echo '$month '. $month;
+echo '$week '. $week;
+echo '$col_today '. $col_today;
+echo '$col_yesterday '. $col_yesterday;
+echo '$col_week '. $col_week;
+echo '$col_week_last '. $col_week_last;
+echo '$col_month '. $col_month;
+echo '$col_month_last '. $col_month_last;
