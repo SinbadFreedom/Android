700e5399a76f6d0aa908d7db0cd90052cea759f9
SinbadFreedom <151229152@qq.com>
2019-05-21 11:47:45 +0800

    1. 修改用户信息

--- build/php/user_info.php
+++ build/php/user_info.php
@@ -77,36 +77,36 @@ if ($user_id) {
             </div>
         </div>
         <div class="form-group row">
-            <label for="staticEmail" class="col-1 col-form-label">昵称:</label>
-            <div class="col-11">
+            <label for="staticEmail" class="col-form-label" style="width: 80px">昵称:</label>
+            <div style="width: 100%">
                 <input type="text" readonly class="form-control-plaintext" id="staticEmail"
                        value="<?php echo $nick_name ?>">
             </div>
         </div>
         <div class="form-group row">
-            <label for="staticEmail" class="col-1 col-form-label">ID:</label>
-            <div class="col-11">
+            <label for="staticEmail" class="col-form-label" style="width: 80px">ID:</label>
+            <div style="width: 100%">
                 <input type="text" readonly class="form-control-plaintext" id="staticEmail"
                        value="<?php echo $user_id ?>">
             </div>
         </div>
         <div class="form-group row">
-            <label for="staticEmail" class="col-1 col-form-label">注册:</label>
-            <div class="col-11">
+            <label for="staticEmail" class="col-form-label" style="width: 80px">注册:</label>
+            <div style="width: 100%">
                 <input type="text" readonly class="form-control-plaintext" id="staticEmail"
                        value="<?php echo date('Y-m-d', $create_time) ?>">
             </div>
         </div>
         <div class="form-group row">
-            <label for="inputPassword" class="col-1 col-form-label">等级:</label>
-            <div class="col-11">
+            <label for="inputPassword" class="col-form-label" style="width: 80px">等级:</label>
+            <div style="width: 100%">
                 <input type="text" readonly class="form-control-plaintext" id="inputPassword"
                        value="<?php echo $level ?>">
             </div>
         </div>
         <div class="form-group row">
-            <label for="inputExp" class="col-1 col-form-label">经验:</label>
-            <div class="col-11">
+            <label for="inputExp" class="col-form-label" style="width: 80px">经验:</label>
+            <div style="width: 100%">
                 <input type="text" readonly class="form-control-plaintext" id="inputExp"
                        value="<?php echo $exp . '/' . $exp_max; ?>">
             </div>
