f042b490a79b395a8da0f9e7269e7c0bc1e06cb6
SinbadFreedom <151229152@qq.com>
2019-05-27 18:33:32 +0800

    1. 修改doc tag button text

--- build/index.php
+++ build/index.php
@@ -6,7 +6,7 @@ session_start();
 <head>
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>熊猫文档-面向程序员的技术文档网站</title>
+    <title>熊猫文档专注于编程技术</title>
     <link rel="stylesheet" href="/lib/bootstrap-4.3.1-dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="/css/dashidan.css">
 </head>
