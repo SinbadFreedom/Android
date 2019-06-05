d7528ab6728cbdd999b69e95c6f3b905456ec86b
SinbadFreedom <151229152@qq.com>
2019-06-05 12:08:13 +0800

    1. 修改回复mustache 替换handlebars

--- build/js/doc.js
+++ build/js/doc.js
@@ -92,8 +92,8 @@ function docLoadNoteSuccess(res) {
     console.log('docLoadNoteSuccess ' + data);
     /** 组合模板文件和数据文件，生成html*/
     // let html = hts2Html(hbs_note, data);
-    let html = Mustache.to_html(hbs_note, data).replace(/^\s*/mg, '');
-    // let html = Mustache.to_html(hbs_note, data);
+    // let html = Mustache.to_html(hbs_note, data).replace(/^\s*/mg, '');
+    let html = Mustache.render(hbs_note, data);
     console.log('docLoadNoteSuccess html ' + html);
     $('#doc_note').html(html);
 }
